6cc3b8b8e3cc262b69457f689a6e02b7b2615998
Omer Sarig <omersarig@hiveware.io>
2026-03-05 22:39:19 +0200

    Use thiserror in the resource module

--- holyrig/src/resources.rs
+++ holyrig/src/resources.rs
@@ -1,7 +1,22 @@
-use anyhow::Result;
 use std::{collections::HashMap, path::PathBuf, sync::Arc};
+use thiserror::Error;
 
+use crate::runtime::parser_errors::ParseError;
 use crate::runtime::{Interpreter, SchemaFile, parse_and_validate_with_schema, parse_schema};
+
+#[derive(Error, Debug)]
+pub enum ResourceError {
+    #[error("Could not find '{0}' in any ancestor of the current directory")]
+    DirNotFound(String),
+    #[error("Could not find config directory")]
+    ConfigDirNotFound,
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Schema(#[from] ParseError),
+    #[error("Rig parse errors:\n{}", .0.iter().map(|e: &ParseError| e.to_string()).collect::<Vec<_>>().join("\n"))]
+    Rig(Vec<ParseError>),
+}
 
 pub struct Resources {
     pub schemas: HashMap<String, SchemaFile>,
@@ -9,18 +24,18 @@ pub struct Resources {
 }
 
 impl Resources {
-    pub fn load() -> Result<Arc<Self>> {
+    pub fn load() -> Result<Arc<Self>, ResourceError> {
         let schemas = Self::load_schemas()?;
         let rigs = Self::load_rig_files(&schemas)?;
         Ok(Arc::new(Self { schemas, rigs }))
     }
 
-    fn load_resources<T, C, F: Fn(PathBuf, &C) -> Result<(String, T)>>(
+    fn load_resources<T, C, F: Fn(PathBuf, &C) -> Result<(String, T), ResourceError>>(
         extension: &[u8],
         dir: &str,
         context: C,
         load_fn: F,
-    ) -> Result<HashMap<String, T>> {
+    ) -> Result<HashMap<String, T>, ResourceError> {
         let base_dir = if cfg!(debug_assertions) {
             let mut candidate = std::env::current_dir()?;
             loop {
@@ -28,14 +43,11 @@ impl Resources {
                     break candidate;
                 }
                 if !candidate.pop() {
-                    anyhow::bail!(
-                        "Could not find '{}' in any ancestor of the current directory",
-                        dir
-                    );
+                    return Err(ResourceError::DirNotFound(dir.to_string()));
                 }
             }
         } else {
-            dirs::config_dir().ok_or_else(|| anyhow::anyhow!("Could not find config directory"))?
+            dirs::config_dir().ok_or(ResourceError::ConfigDirNotFound)?
         };
 
         base_dir
@@ -60,7 +72,7 @@ impl Resources {
             .collect()
     }
 
-    fn load_schemas() -> Result<HashMap<String, SchemaFile>> {
+    fn load_schemas() -> Result<HashMap<String, SchemaFile>, ResourceError> {
         Self::load_resources(b"schema", "schema", (), |path, _| {
             let schema = parse_schema(&std::fs::read_to_string(path)?)?;
             Ok((schema.name.clone(), schema))
@@ -69,11 +81,11 @@ impl Resources {
 
     fn load_rig_files(
         schemas: &HashMap<String, SchemaFile>,
-    ) -> Result<HashMap<String, Interpreter>> {
+    ) -> Result<HashMap<String, Interpreter>, ResourceError> {
         Self::load_resources(b"rig", "rigs", schemas, |path, schemas| {
             let source = std::fs::read_to_string(path)?;
-            // TODO: remove unwrap
-            let rig_file = parse_and_validate_with_schema(&source, schemas).unwrap();
+            let rig_file =
+                parse_and_validate_with_schema(&source, schemas).map_err(ResourceError::Rig)?;
             Ok((rig_file.impl_block.name.clone(), Interpreter::new(rig_file)))
         })
     }
