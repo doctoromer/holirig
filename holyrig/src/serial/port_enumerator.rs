f12a88c45dfa5f401261381168f45e7e8c4764f7
Omer Sarig <omersarig@hiveware.io>
2026-03-06 13:38:55 +0200

    Add error type to port enumerator

--- holyrig/src/serial/port_enumerator.rs
+++ holyrig/src/serial/port_enumerator.rs
@@ -1,12 +1,20 @@
+use thiserror::Error;
 use tokio::sync::mpsc;
 use tokio::time::{Duration, sleep};
 
 use crate::gui::{GuiMessage, SerialPortEntry};
 
-pub async fn run(gui_sender: mpsc::Sender<GuiMessage>) {
+#[derive(Error, Debug)]
+pub enum PortEnumeratorError {
+    #[error("Failed to enumerate serial ports: {0}")]
+    Enumeration(#[from] serialport::Error),
+    #[error("Failed to send ports to GUI: {0}")]
+    Send(#[from] mpsc::error::SendError<GuiMessage>),
+}
+
+pub async fn run(gui_sender: mpsc::Sender<GuiMessage>) -> Result<(), PortEnumeratorError> {
     loop {
-        let ports = serialport::available_ports()
-            .unwrap_or_default()
+        let ports = serialport::available_ports()?
             .into_iter()
             .map(|p| {
                 let display_name = match &p.port_type {
@@ -26,13 +34,7 @@ pub async fn run(gui_sender: mpsc::Sender<GuiMessage>) {
             })
             .collect();
 
-        if gui_sender
-            .send(GuiMessage::AvailablePorts(ports))
-            .await
-            .is_err()
-        {
-            break;
-        }
+        gui_sender.send(GuiMessage::AvailablePorts(ports)).await?;
 
         sleep(Duration::from_secs(2)).await;
     }
